03708002de809326fb185b5ffdc5aeab28dc0e47
Mahmoud Othman <mahmoud20othman2003@gmail.com>
2026-03-12 20:15:37 +0200

    clean repo for deploy

--- dist/routes/roleRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roleRouter = void 0;
-const express_1 = require("express");
-const roleController_1 = require("../controllers/roleController");
-exports.roleRouter = (0, express_1.Router)();
-exports.roleRouter.get("/", roleController_1.listRoles);
-exports.roleRouter.post("/", roleController_1.createRole);
